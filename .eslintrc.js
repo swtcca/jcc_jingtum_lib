6856f4c84c39f81e822890b9200c6ba5f6d269da
Lospring Liu <lospringliu@gmail.com>
2019-03-24 23:30:20 -0400

    prepare collabration: enforce format lint test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,13 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-    root: true,
-    parser: 'babel-eslint',
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: 'module'
-    },
-    env: {
-        browser: true,
-        node: true
-    },
-    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-    extends: 'eslint:recommended',
-    'rules': {
-        'standard/no-callback-literal': [0, ["cb", "callback"]],
-        'space-before-function-paren': 0,
-        'no-useless-constructor': 0,
-        'arrow-parens': 0,
-        'indent': 0,
-        'semi': 0,
-        'quotes': 0,
-        'eol-last': 0,
-        'generator-star-spacing': 0,
-        "camelcase": 0
-    }
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  //extends: ["prettier"],
+  extends: ["eslint:recommended"]
 }
